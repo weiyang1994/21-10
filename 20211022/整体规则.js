87b026611ead8122297bb27f1c73cca50b19f2b5
yjli <yjli@hillinsight.com>
2021-10-28 11:31:00 +0800

    整体规则

---
+++ 20211022/整体规则.js
@@ -0,0 +1,30 @@
+/** 
+ * 1、优先完成有道云里边的复习、练习题；
+ *    完成之后可以来此项目中找练习，feature-exercise 分支上是练习，
+ *    或者index.html中，完成之后做好标记。
+ * 
+ * 2、vscode快捷键
+ *    打开terminal(终端)：ctrl/command + j
+ *    查找文件：ctrl + p
+ *    保存文件： command + s
+ *    保存所有文件： command + option + s
+ *    关闭编辑器窗口 : ctrl + w
+ *    关闭所有窗口： command + k + w
+ *    显示/隐藏左侧目录栏： ctrl + b
+ *    选中文字：shift + left / right / up / down
+ * 
+ *    移动行：alt + up/down
+ *    复制当前行：shift + alt +up/down  或者  command + c
+ *    删除当前行：shift + ctrl + k   或者  command
+ * 
+ * 3、git常用命令
+ *    更新本地库： git fetch
+ *    把新内容合并到当前分支： git rebase origin/main
+ *    提交文件： git add .  或者 git add 文件名或文件夹
+ *    生成提交点： git commit -m  '备注，可中文可英文，最好和当前提交有关联'
+ *    推到远程： git push origin 本地分支名:远程分支名    (这两个分支名可以不一样，也可以一样，这个冒号两端不能用空格)
+ *    查看当前状态： git status
+ *    查看当前的改动： git diff
+ *    查看已提交的改动： git show  提交点的log标识
+ * 
+ */
