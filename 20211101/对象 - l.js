a7d58c43a373f24a160f66920a0770516089200d
yjli <yjli@hillinsight.com>
2021-11-15 23:01:18 +0800

    原型链

--- 20211101/对象 - l.js
+++ 20211101/对象 - l.js
@@ -1,23 +1,30 @@
 // 定义一个构造函数
-function Person() {}
+function Person() {
+  this.age = 25;
+}
 /**
  * 1、每个构造函数都有一个prototype属性，这个属性是一个指针，指向一个对象，称这个对象为原型对象。
  * 这个原型对象上有一个默认属性constructor，该属性也是一个指针，指向其关联的构造函数。
+ * 
+ * ---- 构造函数 和 原型对象 之间的关系
  */
 let prototypeObj = Person.prototype;   //原型对象
 console.log(prototypeObj.constructor === Person);
 /**
  * 通过构造函数创造的实例对象person，拥有一个内部属性__proto__，该属性指向其原型对象，且能访问原型对象上的所有属性和方法。
+ * 
+ * ---- 实例 和 原型对象 之间的关系。
  */
 let person = new Person();
-console.log(person.__proto__ === prototypeObj);
+console.log(person.__proto__ === Person.prototype);
 /**
  * 构造函数所有的属性和方法都是挂在原型对象上的
  */
-prototypeObj.name = '小白兔';
+Person.name1 = '小白兔 - 静态属性'; // ----不能通过 实例 访问
+Person.prototype.name = '小白兔';
 // Person.prototype.name = '小白兔';
-console.log(person.name)
-prototypeObj.sayName = function() {
+console.log(person, person.name, person.age, person.age1, Person.name1)
+Person.prototype.sayName = function() {
   console.log('我的名字是:', this.name);
 }
 person.sayName();
@@ -30,7 +37,9 @@ console.log('--------------------以上是第一部分的内容')
  * 有一个构造函数 Man ，把 Man 的实例对象 赋值给 Person 的原型对象，就关联好了。
  */
  function Man() {}
- Person.prototype = new Man();
+ Person.prototype = new Man();  //两个构造函数之间的纽带
+
+ console.log(Person.prototype.__proto__ === Man.prototype)  // 实例 的 __proto__ 属性指向 其原型对象。
  /**
   * 给 Man 构造函数加一些属性和方法
   */
@@ -45,29 +54,31 @@ console.log('--------------------以上是第一部分的内容')
  /**
   * person1 实例可以访问到 Man 原型对象上的属性和方法
   */
-//  console.log(person1.name1)
-//  person1.sayHello();
+ console.log(person1.name1)
+ person1.sayHello();
  /**
   * person1 上有一个 __proto__ 属性指向其原型对象
   * 此时这个原型对象又是 Man 的实例，所以这个原型对象上有个 __proto__ 指向 Man 的原型对象
   * 所有 person1.__proto__.proto__ 指向 Man 的原型对象
   */
-//  console.log(person1.__proto__ === Person.prototype)
-//  console.log(Person.prototype.__proto__ === Man.prototype)
-//  console.log(person1.__proto__.__proto__ === Man.prototype)
+ console.log(person1.__proto__ === Person.prototype)
+ console.log(Person.prototype.__proto__ === Man.prototype)
+ console.log(person1.__proto__.__proto__ === Man.prototype)
  /**
   * 此时 Man 的原型对象不再是其他对象的实例了，所以 Man 来自于 Object，即 Man 的原型对象 是 Object 的实例
   * Man 的原型对象上有个 __proto__ 指向 Object 的原型对象
   */
-//  console.log(Man.prototype.__proto__ === Object.prototype)
+ console.log(Man.prototype.__proto__ === Object.prototype)
+ console.log(person1.__proto__.__proto__.__proto__ === Object.prototype)
  /**
   * 原型链的终点是 Object，所以 Object 的原型对象不是任何对象的实例
   * Object 的原型对象的 __proto__ 指向 null
   */
-//  console.log(Object.prototype.__proto__)
-//  console.log(Object.prototype.__proto__ === null)
+ console.log(Object.prototype.__proto__)
+ console.log(Object.prototype.__proto__ === null)
+ console.log(person1.__proto__.__proto__.__proto__.__proto__ === null)
  
-//  console.log('--------------------以上是第二部分的内容')
+ console.log('--------------------以上是第二部分的内容')
 
  /**
   * 所有函数，都是 Function 的实例
@@ -75,10 +86,10 @@ console.log('--------------------以上是第一部分的内容')
   * 用 typeof '某对象' 获取类型，当结果是 'function' 的，认为'某对象'是 Function 的实例，
   *   都有一个 __proto__ 属性指向 Function 的原型对象
   */
-//  console.log(typeof Person)
-//  console.log(typeof Object)
-//  console.log(Person.__proto__ === Function.prototype)
-//  console.log(Object.__proto__ === Function.prototype)
+ console.log(typeof Person)
+ console.log(typeof Object)
+ console.log(Person.__proto__ === Function.prototype)
+ console.log(Object.__proto__ === Function.prototype) // ----特殊
 
  /**
   * 普通函数
@@ -93,6 +104,7 @@ console.log('--------------------以上是第一部分的内容')
 const aFun = function(){
   console.log('我是一个匿名函数')
 }
+console.log(aFun.prototype.constructor === aFun, '-----')
 // aFun();
 /**
  * 箭头函数
@@ -110,24 +122,24 @@ const bFun = () => {
 /**
  * 一切都为对象，Function 的原型对象 也是 Object 的实例，所以 Function 的原型对象上有个 __proto__ 指向 Object 的原型对象
  */
-// console.log(Function.prototype.__proto__ === Object.prototype)
+console.log(Function.prototype.__proto__ === Object.prototype)  // ----特殊
 
-//  console.log('--------------------以上是第三部分的内容')
+ console.log('--------------------以上是第三部分的内容')
 
 /**
  * 属性查找机制：我们想要查找某个对象的某个属性，如果我们没有在当前对象中找到，我们便沿着原型链向它的上一级查找，
- *  如果上级没有查找到，再一直向上，知道查到Object.prototype，如果还是没有，就返回undefined。
+ *  如果上级没有查找到，再一直向上，直到查到Object.prototype，如果还是没有，就返回undefined。
  */
 /**
  * 查找一个 person 实例上的 lanYangYang 属性，预期是找不到的，因为 person 实例本身上没有这个属性
  */
-// console.log(person['lanYangYang'])
+console.log(person['lanYangYang'])
 /**
  * 给 Object 的原型对象上设置一个自定义属性： lanYangYang: '18岁了'
  */
-// Object.prototype.lanYangYang = '18岁了';
-// console.log(person['lanYangYang'])
+Object.prototype.lanYangYang = '18岁了';
+console.log(person['lanYangYang'])
 
-// console.log('--------------------以上是第四部分的内容')
+console.log('--------------------以上是第四部分的内容')
 
 
