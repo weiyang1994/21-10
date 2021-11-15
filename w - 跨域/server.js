4d09875fa6acf3c5cc2f8ae367b9a68190aea572
yjli <yjli@hillinsight.com>
2021-11-15 18:21:11 +0800

    提交跨域代码

---
+++ w - 跨域/server.js
@@ -0,0 +1,8 @@
+let express = require('express');
+let app = express()
+app.get('/say', function(req, res) {
+  let { wd, callback } = req.query;
+  console.log(wd, callback)
+  res.send(`${callback}('good luck')`)
+})
+app.listen(3000)
